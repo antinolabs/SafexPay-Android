8965ecad4a79eb4140a5fa43bb31748d32c6f40f
Nadir <alinadir0509@gmail.com>
2020-12-07 12:01:48 +0530

    Added the variables for  card  and upi details

--- safexpay/src/main/java/com/safexpay/android/Utils/SessionStore.java
+++ safexpay/src/main/java/com/safexpay/android/Utils/SessionStore.java
@@ -12,6 +12,12 @@ public class SessionStore {
     public static String PAYMODE_ID = "";
     public static String SCHEME_ID = "";
     public static String EMI_MONTHS = "7";
+    public static String Card_Number = "";
+    public static String Card_Holder_Name = "";
+    public static String Cvv = "";
+    public static String Exp_Date = "";
+    public static String Upi_Id = "";
+
 
     //Success keys
     public static String successOrderId = "";
@@ -30,6 +36,12 @@ public class SessionStore {
         PAYMODE_ID = "";
         SCHEME_ID = "";
         EMI_MONTHS = "7";
+        Card_Number = "";
+        Card_Holder_Name = "";
+        Cvv = "";
+        Exp_Date = "";
+        Upi_Id = "";
+
     }
 
     public static void clearPaymentIds() {
